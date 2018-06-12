15cb88985e2a7d136e9b0f67f8893815cbad22f5
Richard Lindsay <richard@tictocfamily.com>
2018-06-12 22:28:50 +0100

    Setup express server
    Attempt to setup routes

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
-const xray = require('x-ray');
-const request = require('request');
-const fs = require('fs');
-
-const scraper = new xray();
-
-const new_beers = scraper, on_tap_links = scraper, on_tap_link = scraper
+var app = require('express')();
+const xray = require('x-ray')();
 var taps = []
 
-new_beers('https://gruntinggrowler.com/the-beer/?orderby=date', '.product', [{
-  name: 'h2',
-  image: 'img@src',
-  price: '.amount'
-}]).write('new_beers.json');
+app.set('json spaces', 2)
 
-on_tap_links('https://gruntinggrowler.com/the-beer/?orderby=price-desc', '.product_cat-draught-beer', [{
-  link: 'a@href'
-}]).paginate('.next@href')
-(function (err, links) {
-  links.forEach((link) => {
-    on_tap_link(link.link, '.summary', [{
-      name: 'h1',
-      image: 'a@href',
-      price: '.price'
-    }])
-    (function (err, results) {
-      results.forEach((result) => {
-        taps.push(result);
-        if (taps.length === links.length) {
-          fs.writeFile('on_tap.json', JSON.stringify(taps, null, '\t'));
-        }
+app.get('/new_beers', function(req, res) {
+  var new_beers = xray('https://gruntinggrowler.com/the-beer/?orderby=date', '.product', [{
+    name: 'h2',
+    image: 'img@src',
+    price: '.amount'
+  }]).stream();
+  new_beers.pipe(res);
+})
+
+app.get('/on_tap', function(req, res) {
+  var on_tap_links = xray('https://gruntinggrowler.com/the-beer/?orderby=price-desc', '.product_cat-draught-beer', [{
+    link: 'a@href'
+  }]).paginate('.next@href')
+  (function (err, links) {
+    links.forEach((link) => {
+      var on_tap_link = xray(link.link, '.product', [{
+        name: 'h1',
+        image: 'img@src',
+        price: '.price'
+      }])
+      (function (err, results) {
+        results.forEach((result) => {
+          taps.push(result);
+          if (taps.length === links.length) {
+            res.json(taps);
+          }
+        });
       });
     });
   });
+})
+
+
+
+var server = app.listen(3000, function () {
+    console.log("app running on port.", server.address().port);
 });
+
+// new_beers('https://gruntinggrowler.com/the-beer/?orderby=date', '.product', [{
+//   name: 'h2',
+//   image: 'img@src',
+//   price: '.amount'
+// }]).write('new_beers.json');
