a64dbbc90e84801e6d7653faaf465f641410b48c
Richard Lindsay <richard@tictocfamily.com>
2018-06-12 22:56:22 +0100

    Get routes working correctly

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 const xray = require('x-ray')();
-var taps = []
+var new_beers_list = [];
+var taps = [];
 
 app.set('json spaces', 2)
 
@@ -9,8 +10,15 @@ app.get('/new_beers', function(req, res) {
     name: 'h2',
     image: 'img@src',
     price: '.amount'
-  }]).stream();
-  new_beers.pipe(res);
+  }])
+  (function (err, results) {
+    results.forEach((result) => {
+      new_beers_list.push(result)
+      if (new_beers_list.length === results.length) {
+        res.json(new_beers_list)
+      }
+    });
+  });
 })
 
 app.get('/on_tap', function(req, res) {
@@ -19,7 +27,7 @@ app.get('/on_tap', function(req, res) {
   }]).paginate('.next@href')
   (function (err, links) {
     links.forEach((link) => {
-      var on_tap_link = xray(link.link, '.product', [{
+      var on_tap_link = xray(link.link, '#primary', [{
         name: 'h1',
         image: 'img@src',
         price: '.price'
@@ -36,14 +44,6 @@ app.get('/on_tap', function(req, res) {
   });
 })
 
-
-
 var server = app.listen(3000, function () {
-    console.log("app running on port.", server.address().port);
+  console.log("app running on port.", server.address().port);
 });
-
-// new_beers('https://gruntinggrowler.com/the-beer/?orderby=date', '.product', [{
-//   name: 'h2',
-//   image: 'img@src',
-//   price: '.amount'
-// }]).write('new_beers.json');
